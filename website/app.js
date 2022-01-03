19dbf534689eaa2f812426a35db0a79a4cf9c47b
MaroEmad <maromda877@gmail.com>
2022-01-03 13:35:26 +0200

    addedvariables

--- website/app.js
+++ website/app.js
@@ -1,5 +1,5 @@
 /* Global Variables */
-
+const apiKey = 'c6aef926523d47d5e027b20e30bacfbc&units=imperial'
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();

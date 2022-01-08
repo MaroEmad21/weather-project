e824fbb53182c8464cbbe2ec338b81fd70cef040
MaroEmad21 <maromda877@gmail.com>
2022-01-08 16:36:03 +0200

    minorchanges

--- website/app.js
+++ website/app.js
@@ -1,6 +1,6 @@
 /* Global Variables */
 const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
-const apiKey = '&appid=c6aef926523d47d5e027b20e30bacfbc';
+const apiKey = '&appid=c6aef926523d47d5e027b20e30bacfbc&units=imperial';
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
